1656592b8a69b151f856ae0b6e92f3fd33d67464
Miguel Olivares <maog1999>
2020-03-24 23:14:02 -0500

    carga animacion pt1

--- tallerRickAndMorty/src/view/GameScreen.js
+++ tallerRickAndMorty/src/view/GameScreen.js
@@ -3,7 +3,14 @@ class GameScreen{
         this.app; 
         this.fondo= []; 
         this.contador1; 
+        this.fondo2;
 
+    }
+    cargarImagenesGS(){
+        for(let i=1;i<119;i++) {
+            fondo[i]=loadImage("Rick-Lab/lab"+" "+"("+i+").jpg");
+        }
+        fondo2 = loadImage("images/scenary2.jpg");
     }
     pintarFondo1(){
         
