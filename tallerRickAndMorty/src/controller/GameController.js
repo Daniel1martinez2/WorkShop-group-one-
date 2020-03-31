cef21cdabf1cbf620acce7f046f88aab6290caff
AlejandroVal99 <joalevalverde@hotmail.com>
2020-03-31 01:01:05 -0500

    txt
    IntentoTXT

--- tallerRickAndMorty/src/controller/GameController.js
+++ tallerRickAndMorty/src/controller/GameController.js
@@ -1,12 +1,17 @@
 class GameController{
     constructor(){
-        this.logica; 
+        this.logic = new Logic() ;
         this.app; 
+        this.logic.recorrerTXT();
+        console.log("entro al contro");
     }
     dibujar(){
 
     }
     mover(){
         
+    }
+    cargarTXT(){
+        this.logic.cargarTXT();
     }
 }
--- tallerRickAndMorty/src/model/Logic.js
+++ tallerRickAndMorty/src/model/Logic.js
@@ -1,21 +1,25 @@
 class Logic{
     constructor(){
         this.rickAndMorty = new RickYMorty(); 
-        this.txtOriginal=[]; 
+        this.txtOriginal = []; 
         this.arma; 
         this.semillas=[]; 
         this.ganaste ;
-        this.rick;
-        
-     
-
+        this.rick
+        console.log("entro a la logic");
+        this.recorrerTXT();
+    }
+    cargarTXT() {
+        this.txtOriginal = loadStrings("data/Import/cuento.txt");
+    
     }
 
     toqueObjeto(){
 
     }
     recorrerTXT(){
-
+        
+        console.log(this.txtOriginal);
     }
     guardarNuevo(){
         
@@ -31,3 +35,5 @@ class Logic{
  
     
 }
+
+
